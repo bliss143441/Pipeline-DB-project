81241b015fef9b44476dff12347385356f6d2b81
Marc <marcgarnica13@gmail.com>
2017-12-09 15:26:55 +0100

    Timing hashtag view on sql file

--- streaming-postgresql/slidingHashtagsWindows.sql
+++ streaming-postgresql/slidingHashtagsWindows.sql
@@ -1,1 +1,3 @@
-CREATE CONTINUOUS VIEW recent_hashtags WITH (sw = '5 minute') AS SELECT h,arrival_timestamp FROM hashtag_stream;
+CREATE CONTINUOUS VIEW recent_hashtags WITH (sw = '5 minutes') AS SELECT h, COUNT(*) FROM hashtag_stream;
+
+CREATE CONTINUOUS VIEW timing_hashtags WITH (sw = '5 minutes') AS SELECT h, arrival_timestamp FROM hashtag_stream;
