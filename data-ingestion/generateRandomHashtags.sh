b79f28fefe9d1bd11e80dabe8c44d3345284030c
Marc <marcgarnica13@gmail.com>
2017-12-11 18:29:59 +0100

    Modification on the hashtag random generation
     - First randomize which hashtag appears.
     - Randomize then the number of appearances of this hashtag.
     - This makes the data more realistic and less normalized.

--- data-ingestion/generateRandomHashtags.sh
+++ data-ingestion/generateRandomHashtags.sh
@@ -18,10 +18,18 @@ non_random_words=`cat $ALL_NON_RANDOM_WORDS | wc -l`
 i=1
 while [ "$i" -ne 0 ]
 do
-random_number=`od -N3 -An -i /dev/urandom |
+random_word=`od -N3 -An -i /dev/urandom |
 awk -v f=1 -v r="$non_random_words" '{printf "%i\n", f + r * $1 / 16777216}'`
-NEW_WORD=$(sed `echo $random_number`"q;d" $ALL_NON_RANDOM_WORDS)
+random_number_of_words=`od -N3 -An -i /dev/urandom |
+awk -v f=1 -v r="20" '{printf "%i\n", f + r * $1 / 16777216}'`
+NEW_WORD=$(sed `echo $random_word`"q;d" $ALL_NON_RANDOM_WORDS)
+echo $NEW_WORD
+k=1
+while [ "$k" -ne "$random_number_of_words" ]
+do
 echo $NEW_WORD
 RESULT=$(psql -h localhost -p 5432 -d pipeline -c "INSERT INTO hashtag_stream (h) VALUES ('$NEW_WORD');")
+let "k = k + 1"
+done
 sleep $1
 done
