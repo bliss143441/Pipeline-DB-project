16a4142766f3fa091b7bbd842c52b0b20908a8e4
Marc <marcgarnica13@gmail.com>
2017-12-13 20:34:15 +0100

    Angular chart working, graphs easily created

--- data-visualization/public/javascripts/app.js
+++ data-visualization/public/javascripts/app.js
@@ -1,4 +1,24 @@
-angular.module('data-visualization', [])
+angular.module('data-visualization', ["chart.js"])
+
 .controller('mainController', ($scope, $http) => {
 	console.log("Hello world");
+
+	$scope.hashtagData = [];
+	$scope.hashtagLabels = [];
+
+	$http.get('api/hashtagsView')
+	.then(function(success) {
+		console.log(success.data);
+		console.log('here')
+		for (row in success.data) {
+			console.log(success.data[row]);
+			$scope.hashtagData.push(success.data[row].total);
+			$scope.hashtagLabels.push(success.data[row].h);
+		}
+	})
+	.then(function(error) {
+		console.log("Error retrieving hashtags views" + error);
+	});
+
+
 });
